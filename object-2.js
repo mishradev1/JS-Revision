cb03801fd6c0d4bcddaf9de33294478d16c1b408
Dev Mishra <mishradev222004@gmail.com>
2024-08-26 01:18:00 +0530

    .

---
+++ object-2.js
@@ -0,0 +1,17 @@
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "dev"
+}
+
+// const.courseInstructor
+
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+const name = ({instructor}) = {
+
+}
+
